73a2ce8c4f91c726261cba68aa5ac5ef4ca97c46
ciscocode27 <francis.mejiav@gmail.com>
2023-01-08 12:36:07 -0500

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrlApiPokemon: 'https://bp-pokemons.herokuapp.com'
+  baseUrlApiPokemon: 'https://tribu-ti-staffing-desarrollo-afangwbmcrhucqfh.z01.azurefd.net/pkm-msa-evaluation/pokemon'
 };
